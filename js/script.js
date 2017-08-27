078002f3a1c3649f16ac30775a3ffc8f6d701b40
Cyril Tiger <cyriltiger@gmail.com>
2017-08-27 15:45:46 +0200

    Parallax
    Changement du html pour permettre un parallax sur le background

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Permet d'obtenir la hauteur de la fenêtre pour assigner une min-height au header pour un bon affichage de l'image 
 
 var x = $(window).height();
+$(".header-bg").css("min-height", x + "px");
 $("header").css("min-height", x + "px");
 
 // Fermeture du menu mobile quand clic sur lien de la nav ou scroll quand sur mobile
@@ -33,7 +34,7 @@ $(".scrollDown, .nav-link, .navbar-brand").click(function() {
 function parallax(){
 	var scrolltop = window.pageYOffset;
 	$("h1").css("top", scrolltop * 0.3 + "px");	
-	$()
+	$(".header-bg").css("top", scrolltop * 0.5 + "px");
 }
 
 
